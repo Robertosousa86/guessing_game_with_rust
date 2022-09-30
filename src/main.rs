7d7f62e558fb2b610fa4a7745ffe959b17886bb8
Roberto Sousa <robertosousa186@gmail.com>
2022-09-30 18:16:33 -0300

    Making the first step of function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o Número");
+
+    println!("Por favor, digite seu palpite");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Ops... ocorreu um erro ao ler a linha.");
+
+    println!("Você chutou o número: {}", guess);
 }
